99253bfbf0000c325382eaafa982bffaf0a94460
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-01-16 20:47:47 -0300

    Corrigindo erros Linter

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,10 +3,10 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = async (param) => {
-    if (!param) {
-      throw new Error('Termo de busca não informado');
-    }
-    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${param}`);
-    const data = await response.json();
-    return data.results;
+  if (!param) {
+    throw new Error('Termo de busca não informado');
+  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${param}`);
+  const data = await response.json();
+  return data.results;
 };
