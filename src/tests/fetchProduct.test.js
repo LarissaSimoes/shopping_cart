a589f39440313bcc12590aa048a6f58c8ef37e4a
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-01-17 20:38:57 -0300

    Requisito 6

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -7,4 +7,28 @@ describe('Teste a função fetchProduct', () => {
   it('fetchProduct é uma função', () => {
     expect(typeof fetchProduct).toBe('function');
   });
+
+  it('fetch é chamado ao executar fetchProduct', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('fetch é chamado com o endpoint correto ao executar fetchProduct', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenNthCalledWith("https://api.mercadolibre.com/items/MLB1405519561");
+  });
+
+  it('se o retorno da função com o argumento MLB1405519561 é o objeto esperado', async () => {
+    const result = await fetchProduct('MLB1405519561');
+    expect(result).toBe(product);
+  });
+
+  it('se ao chamar a função sem argumento, ela retorna o erro esperado', async () => {
+    try {
+      await fetchProduct()
+    } catch (error) {
+      expect(error).toEqual(new Error ('ID não informado'));
+    }
+  });
+
 });
