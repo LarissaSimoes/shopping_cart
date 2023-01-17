5a8c6539570991a5df28ce2f06c26ecef11271d2
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-01-17 17:51:52 -0300

    Requisitos 3, 4 e 5

--- src/main.js
+++ src/main.js
@@ -1,4 +1,43 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productsSection = document.querySelector('.products');
+
+const addLoading = () => {
+    const loading = document.createElement('div');
+    productsSection.appendChild(loading);
+    loading.innerText = 'carregando...';
+    loading.className = 'loading';
+    return loading;
+};
+
+const errorMessage = () => {
+    const errorMsg = document.createElement('div');
+    document.querySelector('body')
+    .appendChild(errorMsg);
+    errorMsg.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+    errorMsg.className = 'error';
+    return errorMsg;
+};
+
+const createItemList = async () => {
+    try {
+        addLoading();
+        const list = await fetchProductsList('computador');
+        productsSection.innerHTML = '';
+        list.forEach((element) => {
+            const product = createProductElement(element);
+            productsSection.appendChild(product);
+        });
+    } catch {
+        errorMessage();
+    }
+};
+
+window.onload = () => {
+    createItemList();
+  };
