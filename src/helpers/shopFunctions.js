4f4855fe182b7c7efc9f156ff6b614e7af62650f
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-01-18 19:01:10 -0300

    Requisito 8

--- src/helpers/shopFunctions.js
+++ src/helpers/shopFunctions.js
@@ -1,4 +1,5 @@
-import { removeCartID } from './cartFunctions';
+import { removeCartID, saveCartID } from './cartFunctions';
+import { fetchProduct } from './fetchFunctions';
 
 // Esses comentários que estão antes de cada uma das funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
@@ -121,6 +122,16 @@ export const createProductElement = ({ id, title, thumbnail, price }) => {
     'product__add',
     'Adicionar ao carrinho!',
   );
+
+  cartButton.addEventListener('click', async () => {
+    saveCartID(id);
+    const cartProducts = document.querySelector('.cart__products');
+    console.log(cartProducts);
+    const result = await fetchProduct(id);
+    const prodResult = createCartProductElement(result);
+    cartProducts.appendChild(prodResult);
+  });
+
   section.appendChild(cartButton);
 
   return section;
